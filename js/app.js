47a1c96151a1e5d117bce7d5ec4b2d9a6413da49
MuhammadMathori <mathorimuhammad449@gmail.com>
2023-12-27 17:48:31 +0700

    menambahkan fitur shopping-cart

---
+++ js/app.js
@@ -0,0 +1,76 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("menu", () => ({
+    items: [
+      { id: 1, name: "Teh Poci Original", img: "poci.jpg", price: 5000 },
+      { id: 2, name: "Teh Poci Coklat", img: "coklat.jpg", price: 5000 },
+      { id: 3, name: "Teh Poci Mangga", img: "mangga.jpg", price: 5000 },
+      { id: 4, name: "Teh Poci Milk", img: "milk.jpg", price: 5000 },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek apakah ada barang yang sama di dalam cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // jika kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        //jika barang sudah ada cek apakah barang sama atau beda dengan di cart
+        this.items = this.items.map((item) => {
+          //jika barang beda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            //jika barang sudah ada tambah jumalah dan quantity
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // ambil item yang mau di hapus berdasarkan id
+      const cartItem = this.items.find((item) => item.id === id);
+
+      //jika item lebih dari 1
+      if (cartItem.quantity > 1) {
+        //telusuri 1/1
+        this.items = this.items.map((item) => {
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        //jika barang sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+// konfrensi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
