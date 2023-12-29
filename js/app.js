afa0e6d0c119bdfe1d342e1a214e1ae7111eb5a6
MuhammadMathori <mathorimuhammad449@gmail.com>
2023-12-29 21:08:35 +0700

    menambahkan fitur payment dengan WA

--- js/app.js
+++ js/app.js
@@ -66,6 +66,46 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+//form validasion
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+//kirim data ketika tombol checkout di click
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/6281332975747?text=" + encodeURIComponent(message));
+});
+
+//format pesan WA
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  No Hp: ${obj.phone}
+  Data Pesanan
+  ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)})\n`)};
+  TOTAL: ${rupiah(obj.total)}
+  Terima Kasih`;
+};
+
 // konfrensi ke rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
